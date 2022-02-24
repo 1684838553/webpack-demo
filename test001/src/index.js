45e28d167af069b126456274e795df5779151fc8
drunk喵咪 <1684838553@qq.com>
2022-02-24 09:39:21 +0800

    Update index.js

--- test001/src/index.js
+++ test001/src/index.js
@@ -2,6 +2,10 @@ import helloword from "./helloword";
 import imgSrc from './assets/img001.png'
 import logoSvg from './assets/pear.svg'
 import demoText from './assets/demo.txt'
+import './style.css'
+import './style.less'
+import Data from './assets/data.xml'
+import Notes from './assets/data.csv'
 
 helloword()
 
@@ -17,4 +21,18 @@ document.body.appendChild(img1)
 
 const block = document.createElement('div')
 block.textContent = demoText
+block.style.cssText = 'width:300px;height:80px'
+block.classList.add('block-bg')
 document.body.appendChild(block)
+
+
+document.body.classList.add('hello')
+
+const span = document.createElement('span')
+span.classList.add('icon')
+// 相应图标并获取字体编码
+span.innerHTML = '&#xec66;'
+document.body.appendChild(span)
+
+
+console.log(Data, Notes)
