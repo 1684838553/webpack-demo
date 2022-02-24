180f4c534e9a4a6617f5c7a2e0a75c176bad8c15
drunk喵咪 <1684838553@qq.com>
2022-02-24 09:39:05 +0800

    Update helloword.js

--- test001/src/helloword.js
+++ test001/src/helloword.js
@@ -1,5 +1,14 @@
-const helloword = () => {
-    console.log('hello word!!!')
+function getString() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('hello word!!!')
+        }, 2000)
+    })
+}
+
+async function helloword() {
+    const res = await getString()
+    console.log(res)
 }
 
 export default helloword
