88d826a8e7f75fceaba0eb4ad42ed612a5554a95
drunk喵咪 <1684838553@qq.com>
2022-02-25 10:04:06 +0800

    Update another-module.js

--- test001/src/another-module.js
+++ test001/src/another-module.js
@@ -1,18 +1,3 @@
-function getComponent() {
-    // 动态导入
-    /*
-    * 优点：按需加载，节省流量
-    */
-    return import('lodash').then(({ default: _ }) => {
-        const ele = document.createElement('div')
+import _ from 'lodash'
 
-        ele.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
-        return ele
-    })
-}
-
-
-getComponent().then(ele => {
-    document.body.appendChild(ele)
-})
+console.log(_.join(['Another', 'module', 'loaded!'], ' '))
